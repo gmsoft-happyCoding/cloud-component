f6e78c27cc65d797c23cb1f4f33cfc791a0f4275
dou <35251293+bdDandelion@users.noreply.github.com>
2019-10-30 15:24:17 +0800

    添加props fallback，及全局配置组件

--- src/wrapSuspense.tsx
+++ src/wrapSuspense.tsx
@@ -1,10 +1,26 @@
-import React, { Suspense } from "react";
-import Loading from "./Loading";
+import React, { Suspense, useContext, useMemo } from 'react';
+import Loading from './Loading';
+import { AnyProps, SuspenseProps } from './typing.d';
+import { ConfigContext } from './context';
 
-const wrapSuspense = Component => props => (
-  <Suspense fallback={<Loading tip="加载中..." />}>
-    <Component {...props} />
-  </Suspense>
-);
+const wrapSuspense = Component => ({ fallback, ...restProps }: AnyProps & SuspenseProps) => {
+  const context = useContext(ConfigContext);
+
+  const fb = useMemo(() => {
+    if (fallback !== undefined) {
+      return fallback;
+    }
+    if (context && context.fallback !== undefined) {
+      return context.fallback;
+    }
+    return <Loading tip="加载中..." />;
+  }, [context, fallback]);
+
+  return (
+    <Suspense fallback={fb}>
+      <Component {...restProps} />
+    </Suspense>
+  );
+};
 
 export default wrapSuspense;
